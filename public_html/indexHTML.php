4b254ea12181912c57289b86dbdc5690f3cae417
Michael Park <michael511.mp@gmail.com>
2017-02-04 14:46:05 -0500

    wokring home rn

--- public_html/indexHTML.php
+++ public_html/indexHTML.php
@@ -1,3 +1,58 @@
-<div>
-    Hello world!
+<div id="main">
+    <img src="assets/img/main/main.jpg">
+</div>
+<div id="journey">
+    <div class="row">
+        <div class="col-md-offset-5 col-md-7 text-center">
+            <div class="whiteBG">
+                <p>You Can Help Too</p>
+                <hr>
+                <h1>Join the Journey</h1>
+                <button class="btn donate">
+                    Donate
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="whatwedo">
+    <div class="row">
+        <div class="col-md-12 text-center">
+            <h1><b>What We Do</b></h1>
+        </div>
+        <div class="col-md-2 col-md-offset-1">
+            <p>
+                Reading clubs and After School Programs
+            </p>
+        </div>
+        <div class="col-md-2">
+            <p>
+                Sponsorship Programs for Pupils from Under-Served Families
+            </p>
+        </div>
+        <div class="col-md-2">
+            <p>
+                Back to School Initiatives
+            </p>
+        </div>
+        <div class="col-md-2">
+            <p>
+                Trainings and Workshops for Teachers
+            </p>
+        </div>
+        <div class="col-md-2">
+            <p>
+                Construct and Refurbish School Facilities
+            </p>
+        </div>
+        <div class="col-md-12 text-center">
+            <button class="btn btn-default">
+                Learn More
+            </button>
+        </div>
+    </div>
+
+</div>
+<div id="emailus">
+
 </div>
