7778b204f1c9bde902467bc11a8cf45c6f38e9b2
amadrzyk <alexmadrzyk@gmail.com>
2017-02-05 16:47:45 -0500

    Created Contact Us Page, Added Fonts

--- public_html/aboutHTML.php
+++ public_html/aboutHTML.php
@@ -1,3 +1,16 @@
-<div>
-    hello world
+
+<div class="aboutus">
+    <div class="aboutusBG">
+        <div class="caption">
+            <h1>About Us</h1>
+            <div class="captionRectangle"></div>
+        </div>
+    </div>
+
+    <div class="ourMission">
+        <h1>Our Mission:</h1>
+        <br>
+        <p>To Inspire the African Child through educational aids and improve their socio-developmental skills
+            through centered on mental and intellectual programs</p>
+    </div>
 </div>
--- public_html/contactHTML.php
+++ public_html/contactHTML.php
@@ -1,49 +1,50 @@
 
-
-<div class="contactBG">
-    <div class="caption">
-        <h1>Contact Us</h1>
-        <div class="captionRectangle"></div>
+<div class="contactus">
+    <div class="contactusBG">
+        <div class="caption">
+            <h1>Contact Us</h1>
+            <div class="captionRectangle"></div>
+        </div>
     </div>
-</div>
 
-<div class="container contactForm">
-    <div class="row">
-        <p>Please don’t hesistate to get in touch with us. We will get back to you as soon as we can.</p>
-    </div>
-    <div class="row form-group">
-        <!-- name -->
-        <div class="col-md-6">
-            <input type="text" class="form-control" placeholder="Full Name:" id="formName"
-                   required="" data-validation-required-message="Please enter your name." aria-invalid="false">
+    <div class="container contactForm">
+        <div class="row">
+            <p>Please don’t hesistate to get in touch with us. We will get back to you as soon as we can.</p>
         </div>
+        <div class="row form-group">
+            <!-- name -->
+            <div class="col-md-6">
+                <input type="text" class="form-control" placeholder="Full Name:" id="formName"
+                       required="" data-validation-required-message="Please enter your name." aria-invalid="false">
+            </div>
 
-        <!-- email -->
-        <div class="col-md-6">
-            <input type="text" class="form-control" placeholder="Email Address:" id="formEmail"
-                   required="" data-validation-required-message="Please enter your email address." aria-invalid="false">
-        </div>
+            <!-- email -->
+            <div class="col-md-6">
+                <input type="text" class="form-control" placeholder="Email Address:" id="formEmail"
+                       required="" data-validation-required-message="Please enter your email address." aria-invalid="false">
+            </div>
 
-        <!-- phone -->
-        <div class="col-md-6">
-            <input type="text" class="form-control" placeholder="Phone Number:" id="formPhone"
-                   required="" data-validation-required-message="Please enter your phone number." aria-invalid="false">
-        </div>
+            <!-- phone -->
+            <div class="col-md-6">
+                <input type="text" class="form-control" placeholder="Phone Number:" id="formPhone"
+                       required="" data-validation-required-message="Please enter your phone number." aria-invalid="false">
+            </div>
 
-        <!-- subject -->
-        <div class="col-md-6">
-            <input type="text" class="form-control" placeholder="Subject:" id="formSubject"
-                   required="" data-validation-required-message="Please enter the subject." aria-invalid="false">
-        </div>
+            <!-- subject -->
+            <div class="col-md-6">
+                <input type="text" class="form-control" placeholder="Subject:" id="formSubject"
+                       required="" data-validation-required-message="Please enter the subject." aria-invalid="false">
+            </div>
 
-        <!-- message -->
-        <div class="col-md-12">
-            <textarea class="form-control" placeholder="Message:"></textarea>
-        </div>
+            <!-- message -->
+            <div class="col-md-12">
+                <textarea class="form-control" placeholder="Message:"></textarea>
+            </div>
 
-        <!-- submit -->
-        <div class="col-md-12" style="text-align: center">
-            <button type="button">Submit</button>
+            <!-- submit -->
+            <div class="col-md-12" style="text-align: center">
+                <button type="button">Submit</button>
+            </div>
         </div>
     </div>
 </div>
