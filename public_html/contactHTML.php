d9ec7d215ace5c58a0d478247873525afb55b6c4
amadrzyk <alexmadrzyk@gmail.com>
2017-02-05 15:24:41 -0500

    Contact Page Completed

--- public_html/contactHTML.php
+++ public_html/contactHTML.php
@@ -1,7 +1,49 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Michael
- * Date: 2017-01-18
- * Time: 오후 12:40
- */
+
+
+<div class="contactBG">
+    <div class="caption">
+        <h1>Contact Us</h1>
+        <div class="captionRectangle"></div>
+    </div>
+</div>
+
+<div class="container contactForm">
+    <div class="row">
+        <p>Please don’t hesistate to get in touch with us. We will get back to you as soon as we can.</p>
+    </div>
+    <div class="row form-group">
+        <!-- name -->
+        <div class="col-md-6">
+            <input type="text" class="form-control" placeholder="Full Name:" id="formName"
+                   required="" data-validation-required-message="Please enter your name." aria-invalid="false">
+        </div>
+
+        <!-- email -->
+        <div class="col-md-6">
+            <input type="text" class="form-control" placeholder="Email Address:" id="formEmail"
+                   required="" data-validation-required-message="Please enter your email address." aria-invalid="false">
+        </div>
+
+        <!-- phone -->
+        <div class="col-md-6">
+            <input type="text" class="form-control" placeholder="Phone Number:" id="formPhone"
+                   required="" data-validation-required-message="Please enter your phone number." aria-invalid="false">
+        </div>
+
+        <!-- subject -->
+        <div class="col-md-6">
+            <input type="text" class="form-control" placeholder="Subject:" id="formSubject"
+                   required="" data-validation-required-message="Please enter the subject." aria-invalid="false">
+        </div>
+
+        <!-- message -->
+        <div class="col-md-12">
+            <textarea class="form-control" placeholder="Message:"></textarea>
+        </div>
+
+        <!-- submit -->
+        <div class="col-md-12" style="text-align: center">
+            <button type="button">Submit</button>
+        </div>
+    </div>
+</div>
