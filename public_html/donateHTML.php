3cf9b4ebf51d9d6aa797a9221b77c77a00682308
Michael Park <michael511.mp@gmail.com>
2017-02-06 16:03:42 -0500

    donate done..... for now

--- public_html/donateHTML.php
+++ public_html/donateHTML.php
@@ -1,1 +1,89 @@
-hello
+<div class="donateHTML">
+    <div class="donateBG">
+        <div class="caption">
+            <h1>Donate</h1>
+            <div class="captionRectangle"></div>
+        </div>
+    </div>
+    <div class="donateText">
+        <p>
+            Not far from you children still go to school with torn uniforms, with no stationery, no shoes, or school bag.
+        </p>
+        <br>
+        <p>
+            With your donation, you can help support Dolly Children Foundation’s projects today,
+            you are investingin local schools, after-school programs, and decision makers of our
+            future who are key to changing our world.
+        </p>
+        <br>
+        <p>
+            Thank you.
+        </p>
+        <div class="row text-center">
+            <div class="col-md-6">
+                <button class="btn donateGreen"><a>Donate</a></button>
+            </div>
+            <div class="col-md-6">
+                <button class="btn donateGreen"><a>Sponsor a Child</a></button>
+            </div>
+        </div>
+    </div>
+    <div class="makeadiffernece">
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <h1><strong>Make a Difference</strong></h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 imgPadding">
+                <img class="img-responsive" src="assets/img/main/donate1.jpg">
+            </div>
+            <div class="col-md-6">
+                <h2><strong>Children</strong></h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor arcu, sodales eu lacus vel,
+                    pulvinar dapibus odio. Aliquam aliquet consequat rutrum. Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. Integer volutpat neque sed arcu efficitur volutpat. Ut eget dolor sit amet risus
+                    lacinia tempor. Pellentesque dictum blandit ante in lobortis. Nulla facilisi. Suspendisse aliquet
+                    a tortor sit amet sollicitudin. Sed cursus finibus risus, quis posuere purus semper quis.
+                    Vivamus non ex dapibus, pretium nulla at, iaculis arcu. Vivamus commodo purus eget diam
+                    luctus consequat.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <h2><strong>School</strong></h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor arcu, sodales eu lacus vel,
+                    pulvinar dapibus odio. Aliquam aliquet consequat rutrum. Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. Integer volutpat neque sed arcu efficitur volutpat. Ut eget dolor sit amet risus
+                    lacinia tempor. Pellentesque dictum blandit ante in lobortis. Nulla facilisi. Suspendisse aliquet
+                    a tortor sit amet sollicitudin. Sed cursus finibus risus, quis posuere purus semper quis.
+                    Vivamus non ex dapibus, pretium nulla at, iaculis arcu. Vivamus commodo purus eget diam
+                    luctus consequat.
+                </p>
+            </div>
+            <div class="col-md-6 imgPadding">
+                <img class="img-responsive" src="assets/img/main/donate2.jpg">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 imgPadding">
+                <img class="img-responsive" src="assets/img/main/donate3.png">
+            </div>
+            <div class="col-md-6">
+                <h2><strong>Everyday Life</strong></h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor arcu, sodales eu lacus vel,
+                    pulvinar dapibus odio. Aliquam aliquet consequat rutrum. Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. Integer volutpat neque sed arcu efficitur volutpat. Ut eget dolor sit amet risus
+                    lacinia tempor. Pellentesque dictum blandit ante in lobortis. Nulla facilisi. Suspendisse aliquet
+                    a tortor sit amet sollicitudin. Sed cursus finibus risus, quis posuere purus semper quis.
+                    Vivamus non ex dapibus, pretium nulla at, iaculis arcu. Vivamus commodo purus eget diam
+                    luctus consequat.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
