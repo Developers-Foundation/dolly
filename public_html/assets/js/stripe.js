0e0de6e6284a7cc24a0fc20047d810d0e9c2ae75
amadrzyk <alexmadrzyk@gmail.com>
2017-03-07 00:32:44 -0500

    Renamed form variables

--- public_html/assets/js/stripe.js
+++ public_html/assets/js/stripe.js
@@ -4,13 +4,13 @@
 
 Stripe.setPublishableKey('pk_test_QChjrSlxnapDTftVFe5aHKmy');
 $(function() {
-    var $form = $('#payment-form');
-    $form.submit(function(event) {
+    var form = $('#payment-form');
+    form.submit(function(event) {
         // Disable the submit button to prevent repeated clicks:
-        $form.find('.submit').prop('disabled', true);
+        form.find('.submit').prop('disabled', true);
 
         // Request a token from Stripe:
-        Stripe.card.createToken($form, stripeResponseHandler);
+        Stripe.card.createToken(form, stripeResponseHandler);
 
         // Prevent the form from being submitted:
         return false;
@@ -23,8 +23,8 @@ function stripeResponseHandler(status, response) {
 
     if (response.error) { // Problem!
         // Show the errors on the form:
-        $form.find('.payment-errors').text(response.error.message);
-        $form.find('.submit').prop('disabled', false); // Re-enable submission
+        form.find('.payment-errors').text(response.error.message);
+        form.find('.submit').prop('disabled', false); // Re-enable submission
 
     } else { // Token was created!
 
@@ -32,9 +32,9 @@ function stripeResponseHandler(status, response) {
         var token = response.id;
         console.log(token);
         // Insert the token ID into the form so it gets submitted to the server:
-        $form.append($('<input type="hidden" name="stripeToken">').val(token));
+        form.append($('<input type="hidden" name="stripeToken">').val(token));
 
         // Submit the form:
-        $form.get(0).submit();
+        form.get(0).submit();
     }
 };
