f1ba2c44ba04789fd176c3bbdfcfd65d34bb6c98
Nobody Random <iamnobodyrandom@yahoo.com>
2017-04-15 19:08:17 -0400

    refactor

--- public_html/contact.php
+++ public_html/contact.php
@@ -1,5 +1,78 @@
 <?php
-
 include_once("header.php");
-include_once("contactHTML.php");
+?>
+    <div class="contactus">
+        <div class="contactusBG">
+            <div class="caption">
+                <h1>Contact Us</h1>
+                <!--            <div class="captionRectangle"></div>-->
+            </div>
+        </div>
+
+        <div class="container contactForm">
+            <div class="row">
+                <p>Please don’t hesitate to get in touch with us. We will get back to you as soon as we can.</p>
+            </div>
+            <div class="row form-group">
+                <form class="form-email" action="" method="post" data-form-type="nob">
+                    <div class="col-md-6">
+                        <div>
+                            <input type="text" class="form-control form-input-name" placeholder="Your Name">
+                        </div>
+                        <div>
+                            <input type="text" class="form-control form-input-email" placeholder="Your Email">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                            <textarea class="form-control form-input-message" placeholder="Your Message"
+                                      rows="6"></textarea>
+                    </div>
+                    <div class="col-md-12 text-center">
+                        <button class="btn btn-raised btn-primary btn-lg">Send</button>
+                    </div>
+                </form>
+            </div>
+
+            <!--
+            <div class="row form-group">
+
+                <div class="col-md-6">
+                    <input type="text" class="form-control" placeholder="Full Name:" id="formName"
+                           required="" data-validation-required-message="Please enter your name." aria-invalid="false">
+                </div>
+
+
+                <div class="col-md-6">
+                    <input type="text" class="form-control" placeholder="Email Address:" id="formEmail"
+                           required="" data-validation-required-message="Please enter your email address."
+                           aria-invalid="false">
+                </div>
+
+
+                <div class="col-md-6">
+                    <input type="text" class="form-control" placeholder="Phone Number:" id="formPhone"
+                           required="" data-validation-required-message="Please enter your phone number."
+                           aria-invalid="false">
+                </div>
+
+
+                <div class="col-md-6">
+                    <input type="text" class="form-control" placeholder="Subject:" id="formSubject"
+                           required="" data-validation-required-message="Please enter the subject." aria-invalid="false">
+                </div>
+
+
+                <div class="col-md-12">
+                    <textarea class="form-control" placeholder="Message:"></textarea>
+                </div>
+
+
+                <div class="col-md-12" style="text-align: center">
+                    <button type="button">Submit</button>
+                </div>
+            </div> -->
+        </div>
+    </div>
+<?php
 include_once("footer.php");
+?>
