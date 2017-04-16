26604dc80b3e87860283122f669e082ad92c240c
Nobody Random <iamnobodyrandom@yahoo.com>
2017-04-15 21:23:52 -0400

    service update

--- public_html/assets/stripe/stripe.php
+++ public_html/assets/stripe/stripe.php
@@ -2,6 +2,9 @@
 require '../../../vendor/autoload.php';
 use Stripe\Stripe;
 use Stripe\Charge;
+use Parse\ParseClient;
+use Parse\ParseUser;
+use Parse\ParseObject;
 
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     header("HTTP/1.1 403 Forbidden");
@@ -14,7 +17,9 @@ $success = 'success';
 Stripe::setApiKey($_ENV['STRIPE_API_SECRET']);
 
 try {
-    var_dump($_POST);
+    ParseClient::initialize($_ENV["DB_APP_ID"], "", $_ENV["DB_MASTER_KEY"]);
+    ParseClient::setServerURL($_ENV["DB_SERVER_URL"]);
+
     if ($_POST['donation-type'] == "donation") {
         if (!isset($_POST['stripeToken']))
             throw new Exception("The Stripe Token was not generated correctly");
@@ -35,13 +40,26 @@ try {
             "receipt_email" => $_POST['email-address']
         ));
 
-        var_dump($charge);
+        ParseUser::logIn($_ENV["DB_USER"], $_ENV["DB_PASS"]);
+        $donation = new ParseObject("Donation");
+        $donation->set("type", $type);
+        $donation->set("name", $name);
+        $donation->set("phone", $phone);
+        $donation->set("email", $email);
+        $donation->set("zipAddress", $addressZip);
+        $donation->set("token", $token);
+        $donation->set("amount", $amount);
 
-        $success = 'Your payment was successful.';
+        try {
+            $donation->save();
+        } catch (ParseException $ex) {
+            throw new Exception("Failed to submit to db: " . $ex.get_message());
+        }
+
+        $success = 'Your donation was successfully received. Thank You.';
         echo "{'success': true,'message': " . $success . "}";
     }
 } catch (Exception $e) {
-    echo $error;
-    $error = $e->getMessage();
+    echo "{'success': false,'message': " . $e . "}";
 }
 
