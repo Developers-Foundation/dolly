33105ca26c3479c1855b9a8d2a6e5ee1ca4087f7
Michael Park <michael511.mp@gmail.com>
2017-05-09 14:27:01 -0400

    removed lorem and started to put actutal texts

--- public_html/about.php
+++ public_html/about.php
@@ -22,12 +22,14 @@ include_once("header.php");
                 <div class="col-sm-7 col-xs-12 founderTextDiv">
                     <h1>Message from the<br><span class="founderName">Founder</span></h1>
                     <div class="founderNameRectangle"></div>
-                    <p class="founderText">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor arcu, sodales eu lacus vel,
-                        pulvinar dapibus odio. Aliquam aliquet consequat rutrum. Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Integer volutpat neque sed arcu efficitur volutpat. Ut eget dolor sit amet
-                        risus
-                        lacinia tempor.
+                    <p class="founderText" style="text-align: left">
+                        From my experiences, I believe that access to a conducive learning environment, quality
+                        educational programmes, provision of educational aids and financial support to the less -
+                        privileged children of the communities are crucial needs,  and these aforementioned
+                        values remain the core of our foundation.
+                        <br>
+                        <br>
+                        Adedolapo Osuntuyi
                     </p>
                 </div>
                 <div class="col-sm-5 col-xs-12 founderImgDiv">
@@ -44,32 +46,44 @@ include_once("header.php");
                 </div>
                 <div class="col-md-4">
                     <img src="assets/img/icons/book_green.png" alt="icon1" width="100%"/>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor arcu, sodales eu lacus vel,
-                        pulvinar dapibus odio. Aliquam aliquet consequat rutrum. Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Integer volutpat neque sed arcu efficitur volutpat. Ut eget dolor sit amet
-                        risus
-                        lacinia tempor.
+                    <p style="text-align: left">
+                        DCF holds weekly reading club meetings in the public primary schools in communities we serve.
+                        This is to engage the children in activities that will cultivate a reading culture in them.
+                        <br>
+                        <br>
+                        Our choices of books are those that are able to inspire them for excellence, leadership and
+                        increase their literacy abilities. This has resulted to a marked improvement in the interest of
+                        children towards reading and has improved their ability to express themselves.
                     </p>
                 </div>
                 <div class="col-md-4">
                     <img src="assets/img/icons/kids_green.png" alt="icon2" width="100%"/>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor arcu, sodales eu lacus vel,
-                        pulvinar dapibus odio. Aliquam aliquet consequat rutrum. Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Integer volutpat neque sed arcu efficitur volutpat. Ut eget dolor sit amet
-                        risus
-                        lacinia tempor.
+                    <p style="text-align: left">
+                        This is a program put in place to help children whose parents lack the financial backbone
+                        to support their schooling.
+                        <br>
+                        <br>
+                        The plight of these children before our intervention was inability to access desired and
+                        quality education which hindered their learning processes.
+                        <br>
+                        <br>
+                        Currently, the children being sponsored by DCF have improved dramatically in their academic
+                        performance and their outlook to life has been impacted.
                     </p>
                 </div>
                 <div class="col-md-4">
                     <img src="assets/img/icons/backpack_green.png" alt="icon3" width="100%"/>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor arcu, sodales eu lacus vel,
-                        pulvinar dapibus odio. Aliquam aliquet consequat rutrum. Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Integer volutpat neque sed arcu efficitur volutpat. Ut eget dolor sit amet
-                        risus
-                        lacinia tempor.
+                    <p style="text-align: left">
+                        DCF provides and equips children with basic educational tools, school uniforms, shoes, bags etc.
+                        The initiative has also helped in bringing back out of school children to schools by covering
+                        tuition fees, and needs.  This has helped to motivate, as well as boost their confidence,
+                        and participation in school activities.
+                        <br>
+                        <br>
+                        We understand that learning cannot take place in the absence of the required resources.
+                        <br>
+                        <br>
+                        Presently, we have been able to empower over 3,000 children.
                     </p>
                 </div>
             </div>
@@ -77,22 +91,27 @@ include_once("header.php");
                 <div class="col-md-2"></div>
                 <div class="col-md-4">
                     <img src="assets/img/icons/people_green.png" alt="icon4" width="100%"/>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor arcu, sodales eu lacus vel,
-                        pulvinar dapibus odio. Aliquam aliquet consequat rutrum. Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Integer volutpat neque sed arcu efficitur volutpat. Ut eget dolor sit amet
-                        risus
-                        lacinia tempor.
+                    <p style="text-align: left">
+                        We organize trainings and workshops for teachers to bring them up –to- date on 21st century
+                        teaching methods.
+                        <br>
+                        <br>
+                        Teaching skills have dramatically improved and children learn faster and are happier.
+                        These workshops includes Numeracy made easy, 21st century teaching methods, phonics,
+                        understanding your learner e.t.c.
                     </p>
                 </div>
                 <div class="col-md-4">
                     <img src="assets/img/icons/hammer_green.png" alt="icon5" width="100%"/>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc dolor arcu, sodales eu lacus vel,
-                        pulvinar dapibus odio. Aliquam aliquet consequat rutrum. Lorem ipsum dolor sit amet, consectetur
-                        adipiscing elit. Integer volutpat neque sed arcu efficitur volutpat. Ut eget dolor sit amet
-                        risus
-                        lacinia tempor.
+                    <p style="text-align: left">
+                        Since 2015, we have embarked on the “adopt - a - school project” which we carry out in public
+                        primary schools where the school structures are dilapidated. We move in to upgrade the
+                        facilities and equip such schools with the necessary educational materials. Thus far a block of
+                        four classes, a staff office, library and store have been built from scratch.
+                        <br>
+                        <br>
+                        The project has helped in providing a healthy learning for the children that enhance their
+                        academic performance and increase in school enrollment.
                     </p>
                 </div>
                 <div class="col-md-2"></div>
